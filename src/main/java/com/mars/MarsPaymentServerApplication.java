efd8004ea02fe350d9b9f44996fba3beab51cd38
하현준/서버파트/NE <hyunjoon.ha@nhn.com>
2022-11-16 10:47:00 +0900

    initial commit

---
+++ src/main/java/com/mars/MarsPaymentServerApplication.java
@@ -0,0 +1,13 @@
+package com.mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsPaymentServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MarsPaymentServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mars/MarsPaymentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsPaymentServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
