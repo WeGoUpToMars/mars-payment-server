cfd8f1cc513d2d80a903613576dfdf3cb48ec206
yongyongkr <hahahaa_a@naver.com>
2022-12-07 10:04:49 +0900

    feat: 회원 가입 DTO 생성
    Request에서 name과 email은 validation을 가짐

--- src/main/java/com/mars/user/presentation/dto/UserDto.java
+++
@@ -1,5 +0,0 @@
-package com.mars.user.presentation.dto;
-
-public class UserDto {
-
-}
---
+++ src/main/java/com/mars/user/presentation/dto/UserJoinDto.java
@@ -0,0 +1,41 @@
+package com.mars.user.presentation.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserJoinDto {
+
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Builder
+  static class UserJoinRequest {
+
+    @NotBlank(message = "이름은 공백일 수 없습니다.")
+    @Size(min = 1, max = 10, message = "이름은 1 ~ 10자로 구성되어야 합니다.")
+    private String name;
+
+    @Email
+    private String email;
+
+    private String profile;
+  }
+
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Builder
+  static class UserJoinResponse {
+
+    private String name;
+    private String email;
+    private String profile;
+  }
+}
