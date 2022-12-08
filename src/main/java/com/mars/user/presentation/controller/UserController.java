7ed26da0295260437520259dcaceed6020684c1c
yongyongkr <hahahaa_a@naver.com>
2022-12-08 09:59:14 +0900

    fix: api specification

--- src/main/java/com/mars/user/presentation/controller/UserController.java
+++ src/main/java/com/mars/user/presentation/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("mars/users")
+@RequestMapping("/mars/users")
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
