8153f1820092cda6558532ed8f9b0ffe56da49ef
yongyongkr <hahahaa_a@naver.com>
2022-12-08 09:57:48 +0900

    feat: 회원 가입 관련 controller 로직
    UserService의 join 메서드를 호출하고 ResponseEntity에 담아 반환하도록 구성

--- src/main/java/com/mars/user/presentation/controller/UserController.java
+++ src/main/java/com/mars/user/presentation/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.mars.user.presentation.controller;
 
+import com.mars.user.applicaton.service.UserService;
+import com.mars.user.presentation.dto.UserJoinDto;
+import com.mars.user.presentation.dto.UserJoinDto.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("mars/users")
+@Slf4j
+@RequiredArgsConstructor
 public class UserController {
 
+  private final UserService userService;
+
+  @PostMapping(value = "/join", produces = "application/json")
+  public ResponseEntity<Response> join(UserJoinDto.Request request) {
+    return new ResponseEntity<>(userService.join(request), null, HttpStatus.OK);
+  }
 }
