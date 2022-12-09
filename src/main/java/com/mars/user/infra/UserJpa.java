3387b78be2eab7a4dd12499582de92e95f509c61
yongyongkr <hahahaa_a@naver.com>
2022-12-09 23:37:49 +0900

    feat: 계정 아이디로 유저 찾는 로직
    repository interface 및 infrastructure 로직 추가

--- src/main/java/com/mars/user/infra/UserJpa.java
+++ src/main/java/com/mars/user/infra/UserJpa.java
@@ -1,8 +1,10 @@
 package com.mars.user.infra;
 
 import com.mars.user.domain.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpa extends JpaRepository<User, Long> {
 
+  Optional<User> findByAccountId(String accountId);
 }
