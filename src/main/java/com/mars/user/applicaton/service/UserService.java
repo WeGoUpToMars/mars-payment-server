ceb268b64e59ba51aa694805bf51e677db31b7db
yongyongkr <hahahaa_a@naver.com>
2022-12-10 00:14:43 +0900

    feat: login 로직
    - 해당 아이디를 가진 계정이 없는 경우 에러 처리
    - 비밀번호 불일치 시 에러 처리
    - 모두 일치 시 "OK" 응답

--- src/main/java/com/mars/user/applicaton/service/UserService.java
+++ src/main/java/com/mars/user/applicaton/service/UserService.java
@@ -4,6 +4,8 @@ import com.mars.user.domain.entity.User;
 import com.mars.user.domain.repo.UserRepository;
 import com.mars.user.presentation.dto.UserJoinDto;
 import com.mars.user.presentation.dto.UserJoinDto.Response;
+import com.mars.user.presentation.dto.UserLoginDto;
+import com.mars.user.presentation.dto.UserLoginDto.Request;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +22,28 @@ public class UserService {
   @Transactional
   public UserJoinDto.Response join(UserJoinDto.Request request) {
     final var user = User.create(request.getName(), request.getAccountId(), request.getPassword(), request.getEmail(), request.getProfile());
+    // TODO: 아이디 중복 체크
     userRepository.save(user);
     return User.toJoinResponse(user);
+  }
+
+  public String login(UserLoginDto.Request request) {
+    final var user = userRepository.findByAccountId(request.getAccountId()).orElse(null);
+    checkPresence(user);
+    matchPassword(user, request);
+    return "OK";
+  }
+
+  private void matchPassword(final User user, final Request request) {
+    if (!user.getPassword().equals(request.getPassword())) {
+      throw new IllegalArgumentException("아이디 혹은 비밀번호가 잘못 입력되었습니다.");
+    }
+  }
+
+  private void checkPresence(final User user) {
+    if (user == null) {
+      throw new IllegalArgumentException("해당 아이디를 가진 계정이 존재하지 않습니다.");
+    }
   }
 
   public List<Response> findAll() {
