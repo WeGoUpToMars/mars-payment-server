49543f873c64999255a039a0804b49eb5b6ad6fe
yongyongkr <hahahaa_a@naver.com>
2022-12-09 23:33:54 +0900

    test: User validation
    유저 엔티티 생성 시 필드에 대한 validation 테스트 로직 생성

---
+++ src/test/java/com/mars/user/domain/entity/UserTest.java
@@ -0,0 +1,113 @@
+package com.mars.user.domain.entity;
+
+import com.mars.test.MarsTest;
+import java.lang.reflect.InvocationTargetException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+@MarsTest
+class UserTest {
+
+  @Test
+  void validateName() throws NoSuchMethodException {
+    final var user = new User();
+    final var nameValidation = User.class.getDeclaredMethod("validateName", String.class);
+    nameValidation.setAccessible(true);
+
+    assertAll(
+            () -> assertDoesNotThrow(() -> nameValidation.invoke(user, "a")),
+            () -> assertDoesNotThrow(() -> nameValidation.invoke(user, "D")),
+            () -> assertDoesNotThrow(() -> nameValidation.invoke(user, "한")),
+            () -> assertDoesNotThrow(() -> nameValidation.invoke(user, "한afkeAvmd두")),
+            () -> assertDoesNotThrow(() -> nameValidation.invoke(user, "한길동만")),
+            () -> assertDoesNotThrow(() -> nameValidation.invoke(user, "dfEAFfad"))
+    );
+
+    assertAll(
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, "")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, " ")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, "-")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, "1")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, "ab한23")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, "df아니아르dsdsdsDFV")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, "fasen_123")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> nameValidation.invoke(user, "jke 231"))
+    );
+  }
+
+  @Test
+  void validateAccountId() throws NoSuchMethodException {
+    final var user = new User();
+    final var accountIdValidation = User.class.getDeclaredMethod("validateAccountId", String.class);
+    accountIdValidation.setAccessible(true);
+
+    assertAll(
+            () -> assertDoesNotThrow(() -> accountIdValidation.invoke(user, "asdasd")),
+            () -> assertDoesNotThrow(() -> accountIdValidation.invoke(user, "asdasd123")),
+            () -> assertDoesNotThrow(() -> accountIdValidation.invoke(user, "asda12")),
+            () -> assertDoesNotThrow(() -> accountIdValidation.invoke(user, "qwertqwertqwertqwert"))
+    );
+
+    assertAll(
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, "")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, " ")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, "-")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, "112345")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, "asd1")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, "fsdafsdffsafdsfsdfsdfsdfsafsdf2312")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, "fasen_123")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> accountIdValidation.invoke(user, "jke 231"))
+    );
+  }
+
+  @Test
+  void validatePassword() throws NoSuchMethodException {
+    final var user = new User();
+    final var passwordValidation = User.class.getDeclaredMethod("validatePassword", String.class);
+    passwordValidation.setAccessible(true);
+
+    assertAll(
+            () -> assertDoesNotThrow(() -> passwordValidation.invoke(user, "Asdf123!@#$")),
+            () -> assertDoesNotThrow(() -> passwordValidation.invoke(user, "asd1!@#A")),
+            () -> assertDoesNotThrow(() -> passwordValidation.invoke(user, "ASDFqwer1234!@#$")),
+            () -> assertDoesNotThrow(() -> passwordValidation.invoke(user, "123!@#QWE"))
+    );
+
+    assertAll(
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, "")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, " ")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, "-")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, "112345")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, "asd1")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, "fsdafsdffsafdsfsdfsdfsdfsafsdf2312")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, "fasen_123")),
+            () -> assertThrows(InvocationTargetException.class,
+                               () -> passwordValidation.invoke(user, "jke 231"))
+    );
+  }
+}
