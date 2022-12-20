8353b6c8006d312fcd8da7b551804f7e6ad077b1
yongyongkr <hahahaa_a@naver.com>
2022-12-20 10:23:30 +0900

    feat: 유저 도메인 커스텀 에러로 수정

---
+++ src/main/java/com/mars/user/domain/constant/UserExceptionInfo.java
@@ -0,0 +1,29 @@
+package com.mars.user.domain.constant;
+
+import com.mars.common.exception.vo.CustomException;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum UserExceptionInfo {
+
+  NOT_VALID_USER_NAME("한글, 영문 대,소문자로 구성된 1 ~ 10자로 구성되어야 합니다.", HttpStatus.BAD_REQUEST),
+  NOT_VALID_ACCOUNT_ID("영문 소문자와 숫자로 구성된 5자 ~ 20자여야 합니다. (영문자 필수 입력)", HttpStatus.BAD_REQUEST),
+  NOT_VALID_PASSWORD("영문 대,소문자, 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 16자로 구성되어야 합니다.", HttpStatus.BAD_REQUEST);
+
+  private final String message;
+  private final HttpStatus status;
+
+  UserExceptionInfo(String message, HttpStatus status) {
+    this.message = message;
+    this.status = status;
+  }
+
+  public CustomException exception() {
+    return new CustomException(this.message, this.status);
+  }
+
+  public CustomException exception(Throwable cause) {
+    return new CustomException(this.message, this.status, cause);
+  }
+}
