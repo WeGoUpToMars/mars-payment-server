b7e3c1bd402711040206fd0b760bc17465e6c9fa
yongyongkr <hahahaa_a@naver.com>
2022-12-29 10:19:12 +0900

    feat: 결제 요청 DTO

---
+++ src/main/java/com/mars/payment/kakao/dto/InquiryInfo.java
@@ -0,0 +1,27 @@
+package com.mars.payment.kakao.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class InquiryInfo {
+
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  public static class Request {
+
+  }
+
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  public static class Response {
+
+  }
+}
