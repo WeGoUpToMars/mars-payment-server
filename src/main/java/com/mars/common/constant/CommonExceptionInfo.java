bbdf6f1b6b6f2f90c5aff594affde2c2953ff49e
yongyongkr <hahahaa_a@naver.com>
2023-01-05 00:16:18 +0900

    feat: 파싱 커스텀 에러 추가

--- src/main/java/com/mars/common/constant/CommonExceptionInfo.java
+++ src/main/java/com/mars/common/constant/CommonExceptionInfo.java
@@ -10,6 +10,8 @@ public enum CommonExceptionInfo {
   INVALID_INPUT_VALUE("올바르지 않은 입력값입니다.", HttpStatus.BAD_REQUEST),
   INVALID_TYPE_VALUE("올바르지 않은 타입이 입력되었습니다.", HttpStatus.BAD_REQUEST),
   METHOD_NOT_ALLOWED("해당 메서드를 사용할 수 없습니다.", HttpStatus.BAD_REQUEST),
+
+  CANNOT_PARSE_URL_PARAMETER("url parameter 변환 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
   UNKNOWN_EXCEPTION("알 수 없는 에러가 발생했습니다.", HttpStatus.BAD_REQUEST);
 
   private final String message;
