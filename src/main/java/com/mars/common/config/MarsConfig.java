3c000d07daab34f2f16566e8549b5fd543a0a8f7
hjoon <guswns3371@gmail.com>
2023-01-07 20:43:51 +0900

    feat: restTemplate 빈 추가, util 메소드 추가

--- src/main/java/com/mars/common/config/MarsConfig.java
+++ src/main/java/com/mars/common/config/MarsConfig.java
@@ -3,8 +3,13 @@ package com.mars.common.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MarsConfig {
@@ -16,5 +21,12 @@ public class MarsConfig {
   @Bean(name = "objectMapper")
   public ObjectMapper objectMapper() {
     return MAPPER;
+  }
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+    return restTemplateBuilder.requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                              .defaultMessageConverters()
+                              .build();
   }
 }
