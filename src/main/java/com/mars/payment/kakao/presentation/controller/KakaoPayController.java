c976067421c7528a9f6db0936a068894f8356ae0
yongyongkr <hahahaa_a@naver.com>
2023-01-10 10:30:32 +0900

    feat: 카카오 결제 controller 클래스

--- src/main/java/com/mars/payment/kakao/presentation/controller/KakaoPayController.java
+++ src/main/java/com/mars/payment/kakao/presentation/controller/KakaoPayController.java
@@ -1,4 +1,31 @@
 package com.mars.payment.kakao.presentation.controller;
 
+import com.mars.payment.kakao.application.KakaoPayService;
+import com.mars.payment.kakao.presentation.dto.KakaoApprove;
+import com.mars.payment.kakao.presentation.dto.KakaoPrepare;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mars/payments/kakao")
+@Slf4j
 public class KakaoPayController {
+
+  private final KakaoPayService kakaoPayService;
+
+  @PostMapping("/prepare")
+  public ResponseEntity<KakaoPrepare.Response> preparePayment(@RequestBody KakaoPrepare.Request request) {
+    return ResponseEntity.ok(kakaoPayService.preparePayment(request));
+  }
+
+  @PostMapping("/approve")
+  public ResponseEntity<KakaoApprove.Response> approvePayment(@RequestBody KakaoApprove.Request request) {
+    return ResponseEntity.ok(kakaoPayService.approvePayment(request));
+  }
 }
