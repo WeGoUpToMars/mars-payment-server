749a6fb776d6237d84afb5f297593a85451116cb
yongyongkr <hahahaa_a@naver.com>
2023-01-11 22:10:42 +0900

    feat: CORS 문제 해결

---
+++ src/main/java/com/mars/common/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.mars.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:18189", "http://localhost:63342")
+            .allowedMethods(HttpMethod.GET.name(),
+                            HttpMethod.POST.name(),
+                            HttpMethod.PUT.name(),
+                            HttpMethod.PATCH.name(),
+                            HttpMethod.DELETE.name())
+            .allowCredentials(true);
+  }
+}
