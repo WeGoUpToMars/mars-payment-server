0f7aebba038fcde4b20079e088a92427b12de886
yongyongkr <hahahaa_a@naver.com>
2023-01-12 22:44:12 +0900

    fix: cors 설정 변경

--- src/main/java/com/mars/common/config/WebConfig.java
+++ src/main/java/com/mars/common/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:18189", "http://localhost:63342")
+            .allowedOrigins("http://localhost:18189")
             .allowedMethods(HttpMethod.GET.name(),
                             HttpMethod.POST.name(),
                             HttpMethod.PUT.name(),
